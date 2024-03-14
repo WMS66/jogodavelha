20999bb305b14db67b0522a91562b2ed6bed788a
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-14 17:43:32 -0300

    :rocket: feat: create a project

---
+++ src/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,20 @@
+package jogodavelha;
+import javax.swing.*;
+
+public class JogoDaVelha extends JFrame{
+	public JogoDaVelha() {
+		
+		setVisible(true);
+		setTitle("Jogo da Velha");
+		setDefaultCloseOperation(3);
+		setLayout(null);
+		setBounds(350,150,800,600);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new JogoDaVelha();
+	}
+
+}
