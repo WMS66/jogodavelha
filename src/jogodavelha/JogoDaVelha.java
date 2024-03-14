e3cbc004340093671af6641000f97242512733fc
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-14 18:10:00 -0300

    :rocket: feat: create game buttons

--- src/jogodavelha/JogoDaVelha.java
+++ src/jogodavelha/JogoDaVelha.java
@@ -2,6 +2,8 @@ package jogodavelha;
 import javax.swing.*;
 
 public class JogoDaVelha extends JFrame{
+	
+	JButton[] bt = new JButton[9];
 	public JogoDaVelha() {
 		
 		setVisible(true);
@@ -9,12 +11,22 @@ public class JogoDaVelha extends JFrame{
 		setDefaultCloseOperation(3);
 		setLayout(null);
 		setBounds(350,150,800,600);
+		int cont = 0;
+		
+		for (int i=0; i < 3; i++) {
+			for (int j =0; j < 3; j++) {
+				bt[cont] = new JButton();
+				add(bt[cont]);
+				bt[cont].setBounds((100 * i)+60, (100 * j)+60, 95, 95);
+				cont++;
+			}
+		}
 	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-new JogoDaVelha();
+		new JogoDaVelha();
 	}
 
 }
