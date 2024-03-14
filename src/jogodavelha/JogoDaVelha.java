23b93aba024c8c3558cda0d4716ea4d5cf9f4dbf
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-14 18:52:59 -0300

    :rocket: feat: create a new buttons.

--- src/jogodavelha/JogoDaVelha.java
+++ src/jogodavelha/JogoDaVelha.java
@@ -39,6 +39,78 @@ public class JogoDaVelha extends JFrame{
 				}
 			}
 		});
+		bt[1].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[1] == false) {
+					click[1] = true;
+					mudar(bt[1]);
+				}
+			}
+		});
+		bt[2].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[2] == false) {
+					click[2] = true;
+					mudar(bt[2]);
+				}
+			}
+		});
+		bt[3].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[3] == false) {
+					click[3] = true;
+					mudar(bt[3]);
+				}
+			}
+		});
+		bt[4].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[4] == false) {
+					click[4] = true;
+					mudar(bt[4]);
+				}
+			}
+		});
+		bt[5].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[5] == false) {
+					click[5] = true;
+					mudar(bt[5]);
+				}
+			}
+		});
+		bt[6].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[6] == false) {
+					click[6] = true;
+					mudar(bt[6]);
+				}
+			}
+		});
+		bt[7].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[7] == false) {
+					click[7] = true;
+					mudar(bt[7]);
+				}
+			}
+		});
+		bt[8].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				if(click[8] == false) {
+					click[8] = true;
+					mudar(bt[8]);
+				}
+			}
+		});
 	}
 	
 	public void mudar(JButton btn) {
