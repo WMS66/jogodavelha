49471b65f6d8fa7b0f46c2c6dccecd0b98bac67b
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-14 18:31:38 -0300

    :rocket: feat: create action buttons

--- src/jogodavelha/JogoDaVelha.java
+++ src/jogodavelha/JogoDaVelha.java
@@ -1,9 +1,13 @@
 package jogodavelha;
+import java.awt.event.ActionEvent;
+
 import javax.swing.*;
 
 public class JogoDaVelha extends JFrame{
 	
 	JButton[] bt = new JButton[9];
+	boolean xo = false;
+	
 	public JogoDaVelha() {
 		
 		setVisible(true);
@@ -17,11 +21,18 @@ public class JogoDaVelha extends JFrame{
 			for (int j =0; j < 3; j++) {
 				bt[cont] = new JButton();
 				add(bt[cont]);
-				bt[cont].setBounds((100 * i)+60, (100 * j)+60, 95, 95);
+				bt[cont].setBounds((100 * i)+50, (100 * j)+50, 95, 95);
 				cont++;
 			}
 		}
+		bt[0].addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				
+			}
+		});
 	}
+	
 	
 
 	public static void main(String[] args) {
